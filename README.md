4c7c9f95f1ca955bc95cb40a789318dcbcf264ac
Dario Bear Bardales <73848607+Bearbar00008@users.noreply.github.com>
2022-11-10 15:33:04 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# elements-material-ui
+ 
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Button = () : JSX.Element =>{
+
+    return(<>
+        <div>
+            amigos
+        </div>
+    </>
+)}
+
+export default Button
