01c37f3234c649073ac6f4d3c6d3564dd8791ad7
Dario Bear Bardales <73848607+Bearbar00008@users.noreply.github.com>
2022-11-11 12:26:16 -0600

    add styled components

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 const packageJson = require("./package.json");
 
 export default [
@@ -21,14 +24,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" })
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser()
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: ["styled-components"], 
+    globals: { 'styled-components': 'styled' }
   },
 ];
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,12 +1,21 @@
 import React from "react"
+import styled from 'styled-components'
 
+const Puto = styled.h1`
+    width: 10vw;
+    height: 10vw;
+    background-color: red;
+`
 
 const Button = () : JSX.Element =>{
 
     return(<>
         <div>
-            amigos mios
+            amigos
         </div>
+        <Puto>
+            wqeasd
+        </Puto>
     </>
 )}
 
